2942f9c333018930b43916720f2d04a55803428d
yann.hallouard <yann.hal@gmail.com>
2019-12-17 16:26:23 +0100

    update utilsFunction indent problem

--- UtilsFunction.py
+++ UtilsFunction.py
@@ -54,30 +54,30 @@ def import_edge_data(path='', ids=None, size=(256, 256, 3)):
 
 
 class DataBuilding() :
-  
+
   def __init__(self, image_filenames, labels) :
     self.image_filenames = image_filenames
     self.labels = labels
-    
-    
+
+
   def __len__(self) :
     return (np.ceil(len(self.image_filenames) / float(self.batch_size))).astype(np.int)
-  
-  
+
+
   def __getitem__(self, size=None, name='train_%d.pickle', path='./Challenge_Data/TRAIN_Resized/', image_shape=(64, 64, 3)):
     x = self.image_filenames#[:size]
     y = self.labels#[:size]
 
-    new_x = np.concatenate((x[np.where(y==0)][:int(size/2)], 
-                           x[np.where(y==1)][:int(size/2)]), 
+    new_x = np.concatenate((x[np.where(y==0)][:int(size/2)],
+                           x[np.where(y==1)][:int(size/2)]),
                            axis=0)
-    new_y = np.concatenate((y[np.where(y==0)][:int(size/2)], 
-                           y[np.where(y==1)][:int(size/2)]), 
+    new_y = np.concatenate((y[np.where(y==0)][:int(size/2)],
+                           y[np.where(y==1)][:int(size/2)]),
                            axis=0)
-    
+
     x, y = shuffle(new_x, new_y, random_state=0)
-    
-    
+
+
     X_train = np.array([
                resize(imread(path + str(file_name)), image_shape)
                for file_name in tqdm(x)])
@@ -87,72 +87,72 @@ class DataBuilding() :
 
     return [X_train, y]
 
-  class features_extraction():
-    
+class features_extraction():
+
     def __init__(self, X=None):
         self.X = X.reshape(X.shape[0], X.shape[1]*X.shape[2], X.shape[3])
         self.N = X.shape[0]
 
         self.extracted_features = None
-        
-        
-        
-        
+
+
+
+
     def calcul_features(self, features=['global_level',
-                                        'grey_level', 
-                                        'R_mean', 
-                                        'G_mean', 
-                                        'B_mean', 
+                                        'grey_level',
+                                        'R_mean',
+                                        'G_mean',
+                                        'B_mean',
                                         'R_quantille25',
-                                        'G_quantille25', 
-                                        'B_quantille25', 
-                                        'R_quantille75', 
-                                        'G_quantille75', 
-                                        'B_quantille75', 
+                                        'G_quantille25',
+                                        'B_quantille25',
+                                        'R_quantille75',
+                                        'G_quantille75',
+                                        'B_quantille75',
                                         'R_std',
-                                        'G_std', 
+                                        'G_std',
                                         'B_std']):
-        
-        
+
+
         dico = {'global_level': self.Global_level,
-                'grey_level': self.Grey_level, 
-                'R_mean': self.R_mean, 
-                'G_mean': self.G_mean, 
-                'B_mean': self.B_mean, 
+                'grey_level': self.Grey_level,
+                'R_mean': self.R_mean,
+                'G_mean': self.G_mean,
+                'B_mean': self.B_mean,
                 'R_quantille25': self.R_quant25,
-                'G_quantille25': self.G_quant25, 
-                'B_quantille25': self.B_quant75, 
-                'R_quantille75': self.R_quant75, 
-                'G_quantille75': self.G_quant75, 
-                'B_quantille75': self.B_quant75, 
+                'G_quantille25': self.G_quant25,
+                'B_quantille25': self.B_quant75,
+                'R_quantille75': self.R_quant75,
+                'G_quantille75': self.G_quant75,
+                'B_quantille75': self.B_quant75,
                 'R_std': self.R_std,
-                'G_std': self.G_std, 
+                'G_std': self.G_std,
                 'B_std': self.B_std}
-        
+
         result = pd.DataFrame(np.ones((self.N,len(features))))
         result.columns = features
-        
+
         for feature in tqdm(features):
             result[feature] = dico.get(feature, self.fonctAutre)()
-             
+
         self.extracted_features = result
-        
+
         return(result)
-    
-    
-        
+
+
+
     def fonctAutre(self):
         print("Cette feature n'existe pas")
-    
+
     def Global_level(self):
       grey_level = []
       for i in range(self.N):
         img = self.X[i]
 
         grey_level.append(np.mean(img))
-          
+
       return np.array(grey_level)
-    
+
     def Grey_level(self):
       grey_level = []
       for i in range(self.X.shape[0]):
@@ -167,23 +167,23 @@ class DataBuilding() :
                                   (np.mean(img, axis=1)<0.9))[0])/img.shape[0]
 
         grey_level.append(grey_share)
-          
+
       return np.array(grey_level)
-    
+
     def R_mean(self):
       r_mean = []
       for i in range(self.N):
         img = self.X[i,:,0]
         r_mean.append(np.mean(img))
-        
+
       return np.array(r_mean)
-        
+
     def G_mean(self):
       g_mean = []
       for i in range(self.N):
         img = self.X[i,:,1]
         g_mean.append(np.mean(img))
-            
+
       return np.array(g_mean)
 
     def B_mean(self):
@@ -191,7 +191,7 @@ class DataBuilding() :
       for i in range(self.N):
             img = self.X[i,:,2]
             b_mean.append(np.mean(img))
-            
+
       return np.array(b_mean)
 
     def R_quant25(self):
@@ -199,7 +199,7 @@ class DataBuilding() :
       for i in range(self.N):
             img = np.sort(self.X[i,:,0])
             r_quant25.append(img[int(len(img)*0.25)])
-            
+
       return np.array(r_quant25)
 
     def G_quant25(self):
@@ -207,7 +207,7 @@ class DataBuilding() :
       for i in range(self.N):
             img = np.sort(self.X[i,:,1])
             g_quant25.append(img[int(len(img)*0.25)])
-            
+
       return np.array(g_quant25)
 
     def B_quant25(self):
@@ -215,7 +215,7 @@ class DataBuilding() :
       for i in range(self.N):
             img = np.sort(self.X[i,:,2])
             b_quant25.append(img[int(len(img)*0.25)])
-            
+
       return np.array(b_quant25)
 
     def R_quant75(self):
@@ -223,7 +223,7 @@ class DataBuilding() :
       for i in range(self.N):
             img = np.sort(self.X[i,:,0])
             r_quant75.append(img[int(len(img)*0.75)])
-            
+
       return np.array(r_quant75)
 
     def G_quant75(self):
@@ -231,7 +231,7 @@ class DataBuilding() :
       for i in range(self.N):
             img = np.sort(self.X[i,:,1])
             g_quant75.append(img[int(len(img)*0.75)])
-            
+
       return np.array(g_quant75)
 
     def B_quant75(self):
@@ -239,7 +239,7 @@ class DataBuilding() :
       for i in range(self.N):
             img = np.sort(self.X[i,:,2])
             b_quant75.append(img[int(len(img)*0.75)])
-            
+
       return np.array(b_quant75)
 
     def R_std(self):
@@ -247,7 +247,7 @@ class DataBuilding() :
       for i in range(self.N):
             img = self.X[i,:,0]
             r_std.append(np.std(img))
-            
+
       return np.array(r_std)
 
     def G_std(self):
@@ -255,7 +255,7 @@ class DataBuilding() :
       for i in range(self.N):
             img = self.X[i,:,1]
             g_std.append(np.std(img))
-            
+
       return np.array(g_std)
 
     def B_std(self):
@@ -263,5 +263,5 @@ class DataBuilding() :
       for i in range(self.N):
             img = self.X[i,:,2]
             b_std.append(np.std(img))
-            
+
       return np.array(b_std)
